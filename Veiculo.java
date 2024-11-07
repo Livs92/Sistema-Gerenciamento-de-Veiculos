d08dbc1fec05d9090a05972de54d372580a9169f
Lívia Silveira da Rosa <149737602+Livs92@users.noreply.github.com>
2024-11-07 19:22:29 -0300

    Classe abstrata Veiculo

---
+++ Veiculo.java
@@ -0,0 +1,22 @@
+// Classe abstrata Veiculo
+abstract class Veiculo {
+    public String marca;
+    public String modelo;
+    private int ano;
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public abstract String informacoesVeiculo();
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
